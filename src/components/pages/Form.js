58befc268f4e1de1b281a64295ae249dae960687
zahid-soft <zahidsarang95@gmail.com>
2021-12-20 14:36:24 +0530

    form forntend done

--- src/components/pages/Form.js
+++ src/components/pages/Form.js
@@ -1,150 +1,141 @@
-import React from 'react'
-import * as AiIcons from 'react-icons/ai';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import * as AiIcons from "react-icons/ai";
 
-const formfield = () => {
-    return (
-        <div className="feedbackForm__form">
-        <form>
-          <p>
-            <label className="feedbackForm__label">First Name:</label>
-          </p>
-          <p>
-            <input
-              type='text'
-              name='first name'
-              className="feedbackForm__input"
-              placeholder='john'
-            />
-          </p>
-          <p>
-            <label className="feedbackForm__label">Last Name:</label>
-          </p>
-          <p>
-            <input
-              type='text'
-              name='last name'
-              className="feedbackForm__input"
-              placeholder='Doe'
-            />
-          </p>
+const Form = () => {
 
-          <p>
-            <label className="feedbackForm__label">Address:</label>
-          </p>
-          <p>
-            <textarea
-              type='text'
-              name='address'
-              className="feedbackForm__textarea"
-              placeholder='Enter your full postal Address'
-            />
-          </p>
+  const [countries, setCountries] = useState([]);
+  const [text, setText] = useState("");
+  const [suggestion, setSuggestions] = useState([]);
 
-          <p>
-            <label className="feedbackForm__label">Country:</label>
-          </p>
-          <p className="feedbackForm__countryInput">
-            <input
-              type='text'
-              name='country'
-              className="feedbackForm__country"
-              placeholder='India'
-            /><span className="feedback__searchIcon" ><AiIcons.AiOutlineSearch/></span>
-          </p>
+  useEffect(() => {
+    const loadCountries = async () => {
+      const response = await axios.get("https://restcountries.com/v2/all");
+      setCountries(response.data);
+    };
+    loadCountries();
+  }, []);
 
-          <p>
-            <label className="feedbackForm__label">Email ID:</label>
-          </p>
-          <p>
-            <input
-              type='email'
-              name='email'
-              className="feedbackForm__input"
-              placeholder='example@gmail.com'
-            />
-          </p>
+  const searchCountries = (text) => {
+    let matches = [];
+    if (text.length<=0){
+      setSuggestions([]);
+      setText('')
+     
+    } else {
+      matches = countries.filter((country) => {
+        const regex = new RegExp(`${text}`, "gi");
+        return country.name.match(regex);
+      });
+      setText(text);
+      setSuggestions(matches);
+      
+    }
+  };
 
-           
-          <p>
-            <label className="feedbackForm__label">Phone Number:</label>
-          </p>
-          <p>
-            <input
-              type='tel'
-              name='phone number'
-              className="feedbackForm__input"
-              placeholder='123456'
-            />
-          </p>
+  const onSuggestHandler = (text) => {
+    setText(text);
+    setSuggestions([]);
+  };
 
-          <button className="feedbackForm__btn" type="submit">
-            Submit FeedBack
-          </button>
-        </form>
-      </div>
-    )
-}
+  return (
+    <div className="feedbackForm__form">
+      <form>
+        <p>
+          <label className="feedbackForm__label">First Name:</label>
+        </p>
+        <p>
+          <input
+            type="text"
+            name="first name"
+            className="feedbackForm__input"
+            placeholder="john"
+          />
+        </p>
+        <p>
+          <label className="feedbackForm__label">Last Name:</label>
+        </p>
+        <p>
+          <input
+            type="text"
+            name="last name"
+            className="feedbackForm__input"
+            placeholder="Doe"
+          />
+        </p>
 
-export default formfield
+        <p>
+          <label className="feedbackForm__label">Address:</label>
+        </p>
+        <p>
+          <textarea
+            type="text"
+            name="address"
+            className="feedbackForm__textarea"
+            placeholder="Enter your full postal Address"
+          />
+        </p>
 
+        <p>
+          <label className="feedbackForm__label">Country:</label>
+        </p>
+        <p className="feedbackForm__countryInput">
+          <input
+            onChange={(e) => searchCountries(e.target.value)}
+            type="text"
+            name="country"
+            className="feedbackForm__country"
+            placeholder="India"
+            value={text}
+          />
+          <span className="feedback__searchIcon">
+            <AiIcons.AiOutlineSearch />
+          </span>
+        </p>
 
+        {suggestion &&
+          suggestion.map((item, index) => (
+          
+              <div
+              key={index}
+                className="feedbackForm__suggestion"
+                onClick={() => onSuggestHandler(item.name)}
+              >
+              <p> {item.name}</p>
+               
+              </div>
+          ))}
 
+        <p>
+          <label className="feedbackForm__label">Email ID:</label>
+        </p>
+        <p>
+          <input
+            type="email"
+            name="email"
+            className="feedbackForm__input"
+            placeholder="example@gmail.com"
+          />
+        </p>
 
+        <p>
+          <label className="feedbackForm__label">Phone Number:</label>
+        </p>
+        <p>
+          <input
+            type="tel"
+            name="phone number"
+            className="feedbackForm__input"
+            placeholder="123456"
+          />
+        </p>
 
+        <button className="feedbackForm__btn" type="submit">
+          Submit FeedBack
+        </button>
+      </form>
+    </div>
+  );
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const content = {
-//     inputs: [
-//         {
-//             lable: 'First Name:',
-//             name : 'first name',
-//             type : 'text'
-//         },
-//         {
-//             lable : 'Last Name:',
-//             name: 'last name',
-//             type : 'text'
-//         },
-//         {
-//             lable : 'Address:',
-//             name: 'address',
-//             type : 'address'
-//         },
-//         {
-//             lable : 'Country:',
-//             name: 'country',
-//             type : 'text'
-//         },
-//         {
-//             lable : 'Email ID:',
-//             name: 'email id',
-//             type : 'email'
-//         },
-//         {
-//             lable : 'Phone Number:',
-//             name: 'phone number',
-//             type : 'tel'
-//         },
-//     ]
-// }
-
-// export default content
+export default Form;
